a3efe32125cb7c52772c0acb99f9362dca128f40
Victoria Yost <bobby.9202@gmail.com>
2023-06-27 11:56:22 -0400

    create custom mappings
    * set up user config override directory
    * change buffer switching keybinds
    * make neotree open on the right instead of the default left

---
+++ mappings.lua
@@ -0,0 +1,10 @@
+return {
+  n = {
+    ["<Tab>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["<S-Tab>"] = {
+      function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
+      desc = "Previous buffer",
+    },
+  ["<leader>e"] = { "<cmd>Neotree toggle position=right<cr>", desc = "Toggle Explorer" }
+  }
+}
